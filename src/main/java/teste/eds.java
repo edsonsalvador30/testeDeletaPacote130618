06811fa8a599f946c597bd9437f3913747e2aa48
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-06-13 14:53:38 +0000

    Inserido nova classe

---
+++ src/main/java/teste/eds.java
@@ -0,0 +1,21 @@
+package teste;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author edson.fonseca@techne.com.br
+ * @version 1.0
+ * @since 2018-06-13
+ *
+ */
+ 
+public class eds {
+
+	/**
+	 * Construtor
+	 **/
+	public eds (){
+	}
+
+}
